54958daa7d29d82838b50e9afed69bb6a4b23930
CriDos <CriDos@ya.ru>
2016-09-09 20:48:47 +0300

    add

--- CgtClr/CgtClr.h
+++ CgtClr/CgtClr.h
@@ -15,21 +15,21 @@ namespace CgtClr {
 		static BaseCgt ^ref;
 
 		/// <summary>
-		/// Считает количество элементов в контейнере
+		/// Считает количество элементов в контейнере.
 		/// </summary>
 		/// <param name="id_sdk">Идентификатор контейнера</param>
 		/// <returns>Количество элементов в контейнере</returns>
 		virtual int sdkGetCount(int id_sdk) abstract;
 
 		/// <summary>
-		/// Возвращает идентификатор элемента по его Z-положению(индексу) в контейнере
+		/// Возвращает идентификатор элемента по его Z-положению(индексу) в контейнере.
 		/// </summary>
 		/// <param name="index">Z-положение(индекс) элемента</param>
 		/// <returns>Идентификатор элемента</returns>
 		virtual int sdkGetElement(int id, int index) abstract;
 
 		/// <summary>
-		/// Возвращает идентификатор элемента по имени элемента
+		/// Возвращает идентификатор элемента по имени элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор контейнера</param>
 		/// <param name="name">Имя элемента</param>
@@ -37,21 +37,21 @@ namespace CgtClr {
 		virtual int sdkGetElementName(int id, String ^ name) abstract;
 
 		/// <summary>
-		/// Возвращает флаги элемента
+		/// Возвращает флаги элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Флаги элемента</returns>
 		virtual ElementFlags elGetFlag(int id) abstract;
 
 		/// <summary>
-		/// Возвращает количество свойств элемента
+		/// Возвращает количество свойств элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Количество свойств элемента</returns>
 		virtual int elGetPropCount(int id) abstract;
 
 		/// <summary>
-		/// Возвращает указатель на свойство элемента по индексу с порядковым номером из INI
+		/// Возвращает указатель на свойство элемента по индексу с порядковым номером из INI.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="index">Индекс свойства</param>
@@ -67,7 +67,7 @@ namespace CgtClr {
 		virtual bool elIsDefProp(int id, int index) abstract;
 
 		/// <summary>
-		/// Присваиваем элементу уникальное имя
+		/// Присваиваем элементу уникальное имя.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="name">Имя элемента</param>
@@ -75,35 +75,35 @@ namespace CgtClr {
 		virtual int elSetCodeName(int id, String ^ name) abstract;
 
 		/// <summary>
-		/// Возвращает уникальное имя элемента
+		/// Возвращает уникальное имя элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Имя элемента</returns>
 		virtual String ^ elGetCodeName(int id) abstract;
 
 		/// <summary>
-		/// Возвращает имя класса элемента
+		/// Возвращает имя класса элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Имя класса элемента</returns>
 		virtual String ^ elGetClassName(int id) abstract;
 
 		/// <summary>
-		/// Возвращает содержимое поля Sub из конфигурационного INI-файла элемента
+		/// Возвращает содержимое поля Sub из конфигурационного INI-файла элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Содержимое поля Sub</returns>
 		virtual String ^ elGetInfSub(int id) abstract;
 
 		/// <summary>
-		/// Возвращает общее количество видимых точек у элемента
+		/// Возвращает общее количество видимых точек у элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Количество видимых точек</returns>
 		virtual int elGetPtCount(int id) abstract;
 
 		/// <summary>
-		/// Возвращает указатель на точку по её индексу
+		/// Возвращает указатель на точку по её индексу.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="index">Индекс точки</param>
@@ -111,7 +111,7 @@ namespace CgtClr {
 		virtual IntPtr elGetPt(int id, int index) abstract;
 
 		/// <summary>
-		/// Возвращает указатель на точку по её имени
+		/// Возвращает указатель на точку по её имени.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="name">Имя точки</param>
@@ -119,7 +119,7 @@ namespace CgtClr {
 		virtual IntPtr elGetPtName(int id, String ^ name) abstract;
 
 		/// <summary>
-		/// Возвращает индекс класса элемента
+		/// Возвращает индекс класса элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Индекс класса элемента</returns>
@@ -134,21 +134,21 @@ namespace CgtClr {
 		virtual int elGetSDK(int id) abstract;
 
 		/// <summary>
-		/// Проверяет, является ли элемент ссылкой, либо на него ссылаются
+		/// Проверяет, является ли элемент ссылкой, либо на него ссылаются.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Если элемент является ссылкой или на него ссылаются - true, иначе - false</returns>
 		virtual bool elLinkIs(int id) abstract;
 
 		/// <summary>
-		/// Возвращает идентификатор главного элемента(тот, на который ссылаются другие)
+		/// Возвращает идентификатор главного элемента(тот, на который ссылаются другие).
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Идентификатор элемента</returns>
 		virtual int elLinkMain(int id) abstract;
 
 		/// <summary>
-		/// Помещает в переменные "X" и "Y", позицию элемента в редакторе схем
+		/// Помещает в переменные "X" и "Y", позицию элемента в редакторе схем.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="X">Позиция по горизонтали</param>
@@ -156,7 +156,7 @@ namespace CgtClr {
 		virtual void elGetPos(int id, int % X, int % Y) abstract;
 
 		/// <summary>
-		/// Помещает в переменные "W" и "H", размеры элемента
+		/// Помещает в переменные "W" и "H", размеры элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <param name="W">Ширина</param>
@@ -164,7 +164,7 @@ namespace CgtClr {
 		virtual void elGetSize(int id, int % W, int % H) abstract;
 
 		/// <summary>
-		/// Возвращает специальный идентификатор элемента
+		/// Возвращает специальный идентификатор элемента.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
 		/// <returns>Идентификатор элемента</returns>
@@ -172,7 +172,7 @@ namespace CgtClr {
 		virtual int elGetEID(int id) abstract;
 
 		/// <summary>
-		/// Возвращает указатель на точку с которой соединена указанная
+		/// Возвращает указатель на точку с которой соединена указанная.
 		/// </summary>
 		/// <param name="ptr">Указатель на точку</param>
 		/// <returns>Указатель на точку</returns>
@@ -187,28 +187,28 @@ namespace CgtClr {
 		virtual IntPtr ptGetRLinkPoint(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает тип точки
+		/// Возвращает тип точки.
 		/// </summary>
 		/// <param name="ptr">Указатель на точку</param>
 		/// <returns>Тип точки</returns>
 		virtual PointType ptGetType(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает имя точки
+		/// Возвращает имя точки.
 		/// </summary>
 		/// <param name="ptr">Указатель на точку</param>
 		/// <returns>Имя точки</returns>
 		virtual String ^ ptGetName(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает идентификатор родителя точки
+		/// Возвращает идентификатор родителя точки.
 		/// </summary>
 		/// <param name="ptr">Указатель на точку</param>
 		/// <returns>Идентификатор элемента</returns>
 		virtual int ptGetParent(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает индекс точки относительно точек того же типа
+		/// Возвращает индекс точки относительно точек того же типа.
 		/// </summary>
 		/// <param name="ptr">Указатель на точку</param>
 		/// <returns>Индекс точки</returns>
@@ -222,49 +222,49 @@ namespace CgtClr {
 		virtual String ^ pt_dpeGetName(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает тип свойства
+		/// Возвращает тип свойства.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Тип свойства</returns>
 		virtual DataType propGetType(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает имя свойства
+		/// Возвращает имя свойства.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя свойства</returns>
 		virtual String ^ propGetName(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает указатель на значение свойства
+		/// Возвращает указатель на значение свойства.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Указатель на значение</returns>
 		virtual IntPtr propGetValue(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает значение свойства в формате byte
+		/// Возвращает значение свойства в формате byte.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Значение свойства</returns>
 		virtual byte propToByte(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает значение свойства в формате int
+		/// Возвращает значение свойства в формате int.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Значение свойства</returns>
 		virtual int propToInteger(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает значение свойства в формате double
+		/// Возвращает значение свойства в формате double.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Значение свойства</returns>
 		virtual double propToReal(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Возвращает значение свойства в формате <see cref="String"/>.
+		/// Возвращает значение свойства в формате String.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns></returns>
@@ -278,7 +278,7 @@ namespace CgtClr {
 		virtual int resAddFile(String ^ filePath) abstract;
 
 		/// <summary>
-		/// Добавляет иконку из значения свойства в ресурсы.
+		/// Добавляет иконку из свойства в ресурсы.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя временного файла</returns>
@@ -292,28 +292,28 @@ namespace CgtClr {
 		virtual String ^ resAddStr(String ^ str) abstract;
 
 		/// <summary>
-		/// Добавляет поток данных из значения свойства в ресурсы.
+		/// Добавляет поток данных из свойства в ресурсы.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя временного файла</returns>
 		virtual String ^ resAddStream(IntPtr ptr) abstract;
 
 		/// <summary>
-		///  Добавляет Wave звук из значения свойства в ресурсы.
+		///  Добавляет Wave звук из свойства в ресурсы.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя временного файла</returns>
 		virtual String ^ resAddWave(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Добавляет картинку из значения свойства в ресурсы.
+		/// Добавляет картинку из свойства в ресурсы.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя временного файла</returns>
 		virtual String ^ resAddBitmap(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// Добавляет меню из значения свойства в ресурсы.
+		/// Добавляет меню из свойства в ресурсы.
 		/// </summary>
 		/// <param name="ptr">Указатель на свойство</param>
 		/// <returns>Имя временного файла</returns>
@@ -329,90 +329,94 @@ namespace CgtClr {
 		virtual int _Debug(String ^ text, int color) abstract;
 
 		/// <summary>
-		/// Возвращаем значение параметра среды.
+		/// Возвращает значение параметра среды.
 		/// </summary>
 		/// <param name="index">Индекс параметра</param>
 		/// <returns>Параметр среды</returns>
 		virtual Object ^ GetParam(CgtParams index) abstract;
 
 		/// <summary>
-		/// Возвращаем количество элементов в массиве значения.
+		/// Возвращает количество элементов в массиве.
 		/// </summary>
-		/// <param name="ptr">Указатель на значение</param>
+		/// <param name="ptr">Указатель на массив</param>
 		/// <returns>Количество элементов массива</returns>
 		virtual int arrCount(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает тип элементов в массиве.
 		/// </summary>
-		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <param name="ptr">Указатель на массив</param>
+		/// <returns>Тип элементов массива</returns>
 		virtual DataType arrType(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает имя элемента по индексу массива.
 		/// </summary>
-		/// <param name="ptr">Указатель на значение</param>
-		/// <param name="index"></param>
-		/// <returns></returns>
+		/// <param name="ptr">Указатель на массив</param>
+		/// <param name="index">Индекс элемента массива</param>
+		/// <returns>Имя элемента массива</returns>
 		virtual String ^ arrItemName(IntPtr ptr, int index) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает указатель на данные элемента массива значения.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <param name="index"></param>
-		/// <returns></returns>
-		virtual int arrItemData(IntPtr ptr, int index) abstract;
+		/// <param name="index">Индекс элемента массива</param>
+		/// <returns>Указатель на данные элемента массива</returns>
+		[Obsolete]
+		virtual IntPtr arrItemData(IntPtr ptr, int index) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает элемент массива как указатель на свойство со значением элемента массива.
 		/// </summary>
-		/// <param name="ptr">Указатель на значение</param>
-		/// <param name="index"></param>
-		/// <returns></returns>
-		virtual int arrGetItem(IntPtr ptr, int index) abstract;
+		/// <param name="ptr">Указатель на массив</param>
+		/// <param name="index">Индекс элемента массива</param>
+		/// <returns>Указатель на свойство</returns>
+		virtual IntPtr arrGetItem(IntPtr ptr, int index) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает статус отладки запущенной схемы.
 		/// </summary>
 		/// <param name="id">Идентификатор элемента</param>
-		/// <returns></returns>
+		/// <returns>
+		/// Если число больше 0 - отладка включена.
+		/// Если число равно 0 - отладка выключена.
+		/// </returns>
 		virtual int isDebug(int id) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает тип данных значения.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <returns>Тип данных</returns>
 		virtual DataType dtType(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает данные значения в формате String.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <returns>Строка</returns>
 		virtual String ^ dtStr(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает данные значения в формате int.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <returns>Число</returns>
 		virtual int dtInt(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает данные значения в формате double.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <returns>Дробное число</returns>
 		virtual double dtReal(IntPtr ptr) abstract;
 
 		/// <summary>
-		/// 
+		/// Возвращает название шрифта.
 		/// </summary>
 		/// <param name="ptr">Указатель на значение</param>
-		/// <returns></returns>
+		/// <returns>Название шрифта</returns>
 		virtual String ^ fntName(IntPtr ptr) abstract;
 
 		/// <summary>
